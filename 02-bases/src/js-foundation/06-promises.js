ff996e762b56b501df24c8ff9770eb142c4b0179
k474z4 <k474z4.dev@gmail.com>
2024-11-13 10:24:07 -0300

    Fin seccion Desarrollando en node

--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,5 +1,7 @@
+// Callbacks
 // const getPokemonById = (id, callback) => {
 //     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
 
 //     fetch(url).then((response) => {
 //         response.json().then((pokemon) => {
@@ -12,14 +14,71 @@
 
 // }
 
+// ---------------------------------------------------------------------------------------
+
 //Promesas en cadena
-const getPokemonById = (id) => {
+// const getPokemonById = (id) => {
+//     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+//     return fetch(url)
+//         .then((res) => res.json())
+//         // .then(() => { throw new Error('Pokemon no existe') })
+//         .then((pokemon) => pokemon.name);
+
+// }
+
+
+// --------------------------------------------------------------------------------------
+
+// Async - Await
+// const getPokemonById = async (id) => {
+//     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+//     const res = await fetch(url);
+//     const pokemon = await res.json();
+
+//     return pokemon.name;
+
+
+// }
+
+
+// --------------------------------------------------------------------------------------
+
+// Async - Await + plugin http modular
+// const { http } = require("../plugins");
+
+// const getPokemonById = async (id) => {
+//     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+//     const pokemon = await http.get(url);
+
+//     return pokemon.name;
+
+// }
+
+// --------------------------------------------------------------------------------------
+
+// Axios exercise (mi solucion)
+// const axios = require('axios')
+
+// const getPokemonById = async (id) => {
+//     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+//     const pokemon = await axios.get(url);
+
+//     return pokemon.data.name;
+
+// }
+
+// Axios exercise (solucion de la clase)
+const { httpAxios } = require('../plugins/http-client.plugin');
+
+const getPokemonById = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    return fetch(url)
-        .then((res) => res.json())
-        // .then(() => { throw new Error('Pokemon no existe') })
-        .then((pokemon) => pokemon.name);
+    const pokemon = await httpAxios.get(url);
+    return pokemon.name;
 
 }
 
