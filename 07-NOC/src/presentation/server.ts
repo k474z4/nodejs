7c07813577c32da21085b1bae4abdd601e753749
k474z4 <k474z4.dev@gmail.com>
2024-11-29 03:03:38 -0300

    Se agrega funcionalidad para enviar correos y archivos con nodemailer y gmail

--- 07-NOC/src/presentation/server.ts
+++ 07-NOC/src/presentation/server.ts
@@ -1,7 +1,9 @@
+import { envs } from "../config/plugins/envs.plugins";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service.";
 
 // Podemos tener muchas instancias que trabajajen de la misma forma con destinos diferentes
 const fileSystemLogRepository = new LogRepositoryImpl(
@@ -18,7 +20,13 @@ export class Server {
 
         console.log('Server started...');
 
+        console.log(envs.MAILER_EMAIL, envs.MAILER_SECRET_KEY)
+
         // Enviar email
+        const emailService = new EmailService();
+        emailService.sendEmailWithFileSystemLogs(
+            ['fackingclau@gmail.com']
+        );
 
         // CronService.createJob(
         //     '*/5 * * * * *',
