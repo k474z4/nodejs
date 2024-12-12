c65e1f1cde255efade026e0aee7fb7a94db530bc
k474z4 <k474z4.dev@gmail.com>
2024-12-12 07:22:37 -0300

    REST API CRUD

--- 09-RESTWeb/src/presentation/server.ts
+++ 09-RESTWeb/src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -11,11 +12,13 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options) {
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start() {
@@ -27,6 +30,11 @@ export class Server {
         // Public Folder
         this.app.use(express.static(this.publicPath));
 
+        // Routes
+        this.app.use(this.routes);
+
+
+        // SPA cualquier ruta que no haya sido definida pasa por el comodin
         this.app.get('*', (req, res) => {
             const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
